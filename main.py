f4ef8b52c09cad1e5254d18ed36af4f4cdd63ccd
Mani Kandan <mani@bitscrunch.com>
2024-07-05 00:57:26 +0530

    pred

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import tempfile
 from PIL import Image
 from explorer.near import near_app
+from explorer.prediction import pred
 from fraxbridge import Bridge
 from fraxethstack import staking
 from fraxlend import lend
@@ -29,7 +30,7 @@ st.set_page_config(
 
 
 st.sidebar.image("https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZDFjb2UzaWFwMnZqZWE1b2N3Yjc5OTltYzdxM2h5YXY2MWd6MXBxbyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/UAragLbg9oKRfZLThq/giphy.webp")
-action = st.sidebar.radio("What action would you like to take?", ("Analytics","FraxSwap", "FraxLend","FraxEthStaking","FraxBridging"))
+action = st.sidebar.radio("What action would you like to take?", ("Analytics","Prediction","FraxSwap", "FraxLend","FraxEthStaking","FraxBridging"))
 
 def wallet_con():
     with st.sidebar:
@@ -49,6 +50,8 @@ def wallet_con():
 
 if action == "Analytics":
     near_app()
+elif action == "prediction":
+    pred()
 elif action == "FraxLend":
     lend()
 elif action == "FraxSwap":
